6d1578af9f70856cfc00874321a7293b0b2c4a31
Koushal Kumar <koushalkumar2507@gmail.com>
2026-05-17 05:15:14 +0530

    Added SSL for Railway MySQL

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,19 +1,36 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
+
   host: process.env.DB_HOST,
+
   user: process.env.DB_USER,
+
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
 
-db.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('MySQL Connected');
+  database: process.env.DB_NAME,
+
+  port: process.env.DB_PORT,
+
+  ssl: {
+    rejectUnauthorized: false
   }
+
 });
 
-module.exports = db;
+connection.connect((err) => {
+
+  if (err) {
+
+    console.log("Database connection failed");
+    console.log(err);
+
+  } else {
+
+    console.log("Database connected successfully");
+
+  }
+
+});
+
+module.exports = connection;
